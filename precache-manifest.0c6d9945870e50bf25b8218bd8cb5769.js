ab62ae2f5abca78b1741c3eeffb4e58fef1faca8
Serhii Boiko <boyko0043@gmail.com>
2020-08-05 13:07:33 +0300

    Updates

---
+++ precache-manifest.0c6d9945870e50bf25b8218bd8cb5769.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e9f59f16d85277d5fd107df902b42b",
+    "url": "/React-Banking-app/index.html"
+  },
+  {
+    "revision": "5240f282f662e1501eb0",
+    "url": "/React-Banking-app/static/css/main.8b586aea.chunk.css"
+  },
+  {
+    "revision": "1b6b5ff18260f4ee84d2",
+    "url": "/React-Banking-app/static/js/2.feac507f.chunk.js"
+  },
+  {
+    "revision": "2e4f31804d72137a9d9b13db1a0e4557",
+    "url": "/React-Banking-app/static/js/2.feac507f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5240f282f662e1501eb0",
+    "url": "/React-Banking-app/static/js/main.a09f7182.chunk.js"
+  },
+  {
+    "revision": "c0e06190946882866fd1",
+    "url": "/React-Banking-app/static/js/runtime-main.602c5ca9.js"
+  },
+  {
+    "revision": "2bfb316a4620b2fb2575ac4af98a7ad4",
+    "url": "/React-Banking-app/static/media/leaf.2bfb316a.png"
+  },
+  {
+    "revision": "4d523ca4eb1b1c19827df5fffffd11f9",
+    "url": "/React-Banking-app/static/media/photo.4d523ca4.png"
+  }
+]);
